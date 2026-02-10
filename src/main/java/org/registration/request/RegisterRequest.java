7e83342710f158fa4222c3888b4de69fe1189952
Yanina <yledesma@serv.externos.prismamp.com>
2026-02-10 15:28:27 -0300

    password validation

--- src/main/java/org/registration/request/RegisterRequest.java
+++ src/main/java/org/registration/request/RegisterRequest.java
@@ -1,11 +1,16 @@
 package org.registration.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
 public record RegisterRequest(String name,
                               @Email(message = "Invalid email format")
                               String email,
+                              @Pattern(
+                                      regexp = "^[A-Z][a-z]+\\d{2}$",
+                                      message = "La contraseña debe contener al menos 2 dígitos, una letra mayúscula y letras minúsculas"
+                              )
                               String password,
                               List<PhoneRequest> phones) {}
